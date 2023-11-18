7fcfe971b2b518b9d9802f0690eb185a1dfaa654
Denys Podobrii <denys.podobrii@gmail.com>
2023-11-18 18:50:28 +0200

    Create pretty report in main function

--- main.py
+++ main.py
@@ -3,11 +3,19 @@ import string
 def main():
     path_to_book = "books/frankenstein.txt"
     book_contents = read_file(path_to_book)
-    words = count_words(book_contents)
-    letters = count_letters(book_contents)
+    letters_dict = count_letters(book_contents)
+    letters_value = list(letters_dict.values())
+    letters_value.sort(reverse=True)
 
-    print(words)
-    print(letters)
+    print(f"--- Begin report of {path_to_book} ---")
+    print(f"{count_words(book_contents)} words found in the document\n")
+
+    for value in letters_value:
+        print(f"The {get_key_by_value(letters_dict, value)} character was found {value} times")
+
+    print("--- End report ---")
+        
+
 
 def read_file(path):
     with open(path) as file:
@@ -24,6 +32,7 @@ def count_letters(text):
 
     return letters
 
-    
+def get_key_by_value(dict, value):
+    return list(dict.keys())[list(dict.values()).index(value)]
 
 main()
