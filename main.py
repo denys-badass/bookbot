7e827395186c84fc11e160a59ff579f49f95b475
Denys Podobrii <denys.podobrii@gmail.com>
2023-11-18 17:10:08 +0200

    Add variable for count words in book and prints it

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 def main():
-    pass
+    path_to_book = "books/frankenstein.txt"
+    book_contents = read_file(path_to_book)
+    words = book_contents.split()
+    print(len(words))
 
 def read_file(path):
     with open(path) as file:
