91d82475414751a97af4cc5b7a96c51019f54cfb
Denys Podobrii <denys.podobrii@gmail.com>
2024-06-27 18:53:17 +0300

    complete main.py

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-print("hello world")
+def main():
+    path_to_file = './books/frankenstein.txt'
+    text = get_book_text(path_to_file)
+    print(text)
 
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+main()
